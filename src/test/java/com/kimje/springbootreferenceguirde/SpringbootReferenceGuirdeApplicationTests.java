66f92e03eaafb4a3a49376f969c93eaff86ecc22
kimjinhoo <wlsgnwkd22@naver.com>
2025-06-21 11:27:38 +0900

    스프링부트 application 지연 초기화

---
+++ src/test/java/com/kimje/springbootreferenceguirde/SpringbootReferenceGuirdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimje.springbootreferenceguirde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReferenceGuirdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
